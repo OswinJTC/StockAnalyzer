c5d726ab69ed41fd5e83db977568c0bd68aebed1
OswinJTC <ch0005ai@e.ntu.edu.sg>
2024-03-27 17:23:07 +0800

    v6

--- src/main/java/com/StockAnalyser/StockAnalyser/StockAnalyserApplication.java
+++ src/main/java/com/StockAnalyser/StockAnalyser/StockAnalyserApplication.java
@@ -2,12 +2,22 @@ package com.StockAnalyser.StockAnalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class StockAnalyserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockAnalyserApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello Oswin Chen Jui Tai!";
+
 	}
 
 }
